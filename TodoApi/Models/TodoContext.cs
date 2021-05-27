3c921c5bc1542d4729b1102333c3484ba578cce9
Vinod Patidar <pvinod813@gmail.com>
2021-05-27 11:35:36 +0530

    Initial check-in of module TodoApi

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public TodoContext()
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
